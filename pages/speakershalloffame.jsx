3cf2dd3d406cf3ea3a9da1e2b8aa87174810cb1f
Daniel Holmlund <agnathan@gmail.com>
2023-05-23 14:13:08 -0700

    changed the speakers hall of fame page

--- pages/speakershalloffame.jsx
+++ pages/speakershalloffame.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
+import { CallToAction } from '@/components/CallToAction'
 
 const speakers = [
     {
@@ -88,30 +89,32 @@ const speakers = [
 
 export default function SpeakerHallofFamePage() {
     return (
-        <div className="bg-gray-900 py-24 sm:py-32">
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="mx-auto max-w-2xl lg:mx-0">
-                    <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Speaker Hall of Fame</h2>
-                    <p className="mt-6 text-lg leading-8 text-gray-300">
-                        The Alternative Investing Club invites speakers learn about real estate and alternative investing strategies every Friday.
-                    </p>
-                </div>
-                <ul
-                    title="list"
-                    className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4"
-                >
-                    {speakers.map((speaker) => (
-                        <li key={speaker.name}>
-                            <Link href={speaker.videoUrl}>
-                                <img className="aspect-[14/13] w-full rounded-2xl object-cover" src={speaker.imageUrl} alt="" />
-                                <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-white">{speaker.name}</h3>
-                                <p className="text-base leading-7 text-gray-300">{speaker.title}</p>
-                                <p className="text-sm leading-6 text-gray-500">{speaker.meeting_date}</p>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
+        // <div className="bg-gray-900 py-24 sm:py-32">
+        //     <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        //         <div className="mx-auto max-w-2xl lg:mx-0">
+        //             <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Speaker Hall of Fame</h2>
+        //             <p className="mt-6 text-lg leading-8 text-gray-300">
+        //                 The Alternative Investing Club invites speakers learn about real estate and alternative investing strategies every Friday.
+        //             </p>
+        //         </div>
+        //         <ul
+        //             title="list"
+        //             className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4"
+        //         >
+        //             {speakers.map((speaker) => (
+        //                 <li key={speaker.name}>
+        //                     <Link href={speaker.videoUrl}>
+        //                         <img className="aspect-[14/13] w-full rounded-2xl object-cover" src={speaker.imageUrl} alt="" />
+        //                         <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-white">{speaker.name}</h3>
+        //                         <p className="text-base leading-7 text-gray-300">{speaker.title}</p>
+        //                         <p className="text-sm leading-6 text-gray-500">{speaker.meeting_date}</p>
+        //                     </Link>
+        //                 </li>
+        //             ))}
+        //         </ul>
+        //     </div>
+        // </div>
+
+        <CallToAction />
     )
 }
