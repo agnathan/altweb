6cfe4d21942c31b454c4853017e77957c2ee7d15
Daniel Holmlund <agnathan@gmail.com>
2023-06-16 14:07:52 -0700

    Speaker Hall of Fame will show the current days speaker if the video and photos are posted

---
+++ pages/aboutus.jsx
@@ -0,0 +1,10 @@
+import { PublicLayout } from '@/components/layouts/PublicLayout'
+
+export default function Home({ meetings }) {
+    console.log(meetings)
+    return (
+        <PublicLayout>
+            <h1>About the Alternative Investing Club and Our Beginnings</h1>            
+        </PublicLayout>
+    )
+}
